0ab113a479ba6f1d58ba82004dacb11e997bb286
Eryx-Lopez <122893188+Eryx-Lopez@users.noreply.github.com>
2023-03-14 09:56:38 -0600

    Constructores

--- src/MarzoTres.java
+++ src/MarzoTres.java
@@ -1,8 +1,14 @@
+import java.util.Objects;
+
 public class MarzoTres {
 
     //Para encapsular se tienen que poner los atributos como privadas
     private String nombre;
     private int edad;
+
+    public static void main(String[] args) {
+
+    }
     private String matricula;
     //De ahí click derecho en Generate... y luego a Getter y Setter
     //Se seleccionan los atributos y se genera lo de la parte de abajo
@@ -15,11 +21,27 @@ public class MarzoTres {
         this.nombre = nombre;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MarzoTres)) return false;
+        MarzoTres marzoTres = (MarzoTres) o;
+        return edad == marzoTres.edad && nombre.equals(marzoTres.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, edad);
+    }
+
     public int getEdad() {
         return edad;
     }
 
     public void setEdad(int edad) {
+        if (edad < 0){
+            throw new IllegalArgumentException("Edad negativa no aceptada");
+        }
         this.edad = edad;
     }
 
@@ -43,3 +65,5 @@ public class MarzoTres {
     final int LIMITE_DE_MATERIAS = 10;
 
 }
+
+//class IllegalArgumentException{}
